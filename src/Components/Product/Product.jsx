dd1cdd9c49ee9b192c32f30c97d29dc17da2b462
Tsigereda Kereta <kereta.tw@gmail.com>
2024-11-29 03:34:59 -0800

    single product component add

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import classes from "./Product.module.css"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ProductCard from "./ProductCard"
 
 const Product = () => {
+const [product, setProduct] = useState([]);
+useEffect(()=>{
+    axios
+    .get("https://fakestoreapi.com/products")
+    .then((res)=>{
+        console.log(res)
+        setProduct(res.data);
+    }).catch((err)=>{
+        console.log(err)
+    });
+}, []);
+
   return (
-    <div>
-          
-
-
-
-    </div>
-  )
+   <div className={classes.product_container}>
+{
+product.map((singleProduct, i)=>{
+    return <ProductCard product ={singleProduct} key ={i} />
+})
+}
+</div>
+)
 }
 
 export default Product
--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat"
+import classes from "./Product.module.css"
+import { Rating } from "@mui/material"
 
-const ProductCard = () => {
+
+const ProductCard = ({product}) => {
+  const {image, title, id, price, rating} = product;
   return (
-    <div>
-        <a href=''>
-            <img src='' alt =""/>
+    <div className={classes.card_container}>
+        <a href='/'>
+            <img src={image} alt =""/>
           </a>
           <div>
-               <h3> title</h3>
-               
-
-
-
-
+               <h3>{title}</h3>
+               <div className={classes.rating}>
+                {/* rating */}
+                <Rating value={rating.rate} precision={0.1}/>
+                {/* count */}
+                <small>  {rating.count}</small>
+               </div>
+               <div>
+                  {/* price */}
+                  <CurrencyFormat amount={price} />
+               </div>
+               <button className={classes.button}> add to cart</button>
           </div>
-
-
-
-
-
-
-
-
 
     </div>
   )
