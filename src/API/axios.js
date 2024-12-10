6591f1ff88f97ccbbb9d72eca7466ebb3643f34f
Tsigereda Kereta <kereta.tw@gmail.com>
2024-12-09 20:47:40 -0800

    update deploy

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/clone-f289a/us-central1/app",
+    baseURL:"http://localhost:2011",
 });
 
 
